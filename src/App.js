d8f006e07523ef3e9692ec7e8f5fddb582dc3f36
Akash-Panigrahi <akashpanigrahi96@gmail.com>
2021-03-07 14:29:44 +0530

    completed functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import "./App.css";
+import Thankyou from "./components/Thankyou";
 
 function App() {
+  const [phone, setPhone] = useState("");
+  const [phoneValid, setPhoneValid] = useState("");
+  const [phonePresent, setPhonePresent] = useState(
+    localStorage.getItem("phone")
+  );
+
+  const checkPhoneValidity = (e) => {
+    const indianPhoneNumberRegex = /^(?:(?:\+|0{0,2})91(\s*[ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/gm;
+    if (indianPhoneNumberRegex.test(e.target.value)) {
+      setPhoneValid(true);
+      setPhone(e.target.value);
+    }
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    localStorage.setItem("phone", phone);
+    setPhonePresent(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      {phonePresent ? (
+        <Thankyou phone={phone} />
+      ) : (
+        <form onSubmit={submitForm}>
+          <input
+            name="phone"
+            type="text"
+            defaultValue={phone}
+            onChange={checkPhoneValidity}
+          />
+          <button type="submit" disabled={!phoneValid}>
+            Wow! Get my Paytm Gift Card
+          </button>
+        </form>
+      )}
     </div>
   );
 }
---
+++ src/components/Thankyou.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Thankyou({ phone }) {
+  return <div>Thankyou! You will receive your gift card on {phone} number</div>;
+}
